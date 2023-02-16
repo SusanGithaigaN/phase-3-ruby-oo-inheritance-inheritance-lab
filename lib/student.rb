17520dceba70c8d2d8bbbf81089676159d44c3df
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-16 04:08:18 +0300

    commit tree 43b380682c96b32839a63157139b91f5dd364098

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,13 @@
 require_relative './user'
 class Student < User
     def initialize
-        @knowledge =[]
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+    def knowledge
+        @knowledge
     end
 end
